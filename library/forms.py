6b10cd1fe2e5f16ff8a0ae04fec039440ab672f7
mehddi <02.oudaoud@gmail.com>
2020-05-17 04:56:13 +0100

    **

--- library/forms.py
+++ library/forms.py
@@ -5,4 +5,4 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        exclude = ('timestamp' , 'DOI' ,'owner')
+        exclude = ('timestamp' ,'owner')
